7e2674d3824d958fed1d05f5c0daf078e80bc236
Kaio F. G. Lazaro <kaiolazaro36@hotmail.com>
2023-03-28 18:57:17 -0300

    Layout implementado

---
+++ src/layout/Default.js
@@ -0,0 +1,68 @@
+import { Drawer, List, ListItem, ListItemIcon, ListItemText, Typography } from '@mui/material';
+
+import * as React from 'react';
+
+import Toolbar from '@mui/material/Toolbar';
+import Divider from '@mui/material/Divider';
+
+import ListItemButton from '@mui/material/ListItemButton';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+
+import './Default.scss';
+
+const drawerWidth = 210;
+
+function Layout({ children }) {
+  return (
+    <div className="default">
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Toolbar>Processamento de dados </Toolbar>
+        <Divider />
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText primary="Início" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+        <Divider />
+        {/* TODO: Dashboards personalizadas */}
+        {/* <Toolbar>Minhas dashboards</Toolbar>
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List> */}
+      </Drawer>
+      <main
+        className="content px-3 py-3"
+      >
+        <div />
+        {children}
+      </main>
+    </div>
+  );
+}
+
+export default Layout;
