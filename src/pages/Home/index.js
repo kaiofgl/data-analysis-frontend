3761bbde26902f90be590f948e35f7d742fdb877
Kaio F. G. Lazaro <kaiolazaro36@hotmail.com>
2023-03-30 19:21:32 -0300

    Ajuste home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,12 +2,27 @@ import Layout from '../../layout/Default';
 
 import './Home.scss'
 
+import CardFile from '../../components/Card/CardFile';
+
 const Home = () => {
-    return <Layout>
-        <div className='home'>
-            <div className='h5 fw-100'>Dashboard</div>
-        </div>
-    </Layout>
+
+
+    function handleFile(file) {
+    }
+
+
+    return (
+        <Layout>
+            <div className='home'>
+                <div className='d-flex justify-center h3 fw-100'>Dashboard</div>
+                <div className='d-flex justify-between' >
+                    <div className='col-6' sx={{ minHeight: "300px" }}>
+                        <CardFile onChange={handleFile} />
+                    </div>
+                </div>
+            </div>
+        </Layout >
+    )
 }
 
 export default Home;
