3e67507616dc8e722ec63c7e932bd4192c2d17df
Kaio F. G. Lazaro <kaiolazaro36@hotmail.com>
2023-03-30 19:29:02 -0300

    Ajustes app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import RoutesDefault from './routes';
 
+import theme from './theme';
+
+import { ThemeProvider } from '@mui/material';
+
 import './sass/App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
     return (
         <>
-            <RoutesDefault />
+            <ThemeProvider theme={theme}>
+                <RoutesDefault />
+            </ThemeProvider>
         </>
     );
 }
-
 export default App;
