75ce9ffbdf66f53d7672f2ab7a3395550f6c3238
Kaio F. G. Lazaro <kaiolazaro36@hotmail.com>
2023-04-02 16:22:30 -0300

    Implementado modal de preview

---
+++ src/components/Modal/ModalPreview.js
@@ -0,0 +1,45 @@
+import "./ModalPreview.scss";
+
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+
+import { PieChart } from 'react-feather';
+
+import BarGraph from "../../Graphs/bar";
+
+const ModalPreview = (props) => {
+    const { data, open, column } = props;
+
+    function onClose() {
+        props.onClose();
+    }
+
+    function onConfirm() {
+        props.onConfirm();
+    }
+
+    return (
+        <Dialog
+            open={open}
+            onClose={onClose}
+            className='modal-preview'
+        >
+            <DialogTitle className="d-flex justify-center" sx={{ fontWeight: "bold" }}>
+                {column} <div className='px-2'><PieChart /></div>
+            </DialogTitle>
+            <DialogContent>
+                <div className='bar'>
+                    <BarGraph processed={data} sheetName={column} />
+                </div>
+            </DialogContent>
+            <DialogActions>
+                <Button variant="outlined" color="secondary" status="danger" onClick={onClose}>Fechar</Button>
+            </DialogActions>
+        </Dialog>
+    )
+}
+
+export default ModalPreview;
